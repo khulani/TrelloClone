2b415abd2ff12b16974a36bed851125737b92f98
Khulani Malone <khulani@gmail.com>
2015-02-18 16:47:37 -0800

    allowed secrets for repo upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloClone::Application.config.secret_key_base = '97767e44a5c3825622807f9b8a26bbfb1391fa865a8a47acb489f161e4039975a4abaa59c85b0a6454386df32fd7f24f66ede9cd8faf4af925d46b9d0c28b17c'
